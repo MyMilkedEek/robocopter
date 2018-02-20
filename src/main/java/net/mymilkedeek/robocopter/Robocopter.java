e2b9d226ea8540b17ed8c33558dc9c6e6398bacc
Michael Demey <michael.demey@itextpdf.com>
2018-02-20 20:32:59 +0100

    trying to hit the other one

--- src/main/java/net/mymilkedeek/robocopter/Robocopter.java
+++ src/main/java/net/mymilkedeek/robocopter/Robocopter.java
@@ -1,16 +1,48 @@
 package net.mymilkedeek.robocopter;
 
 import robocode.Robot;
+import robocode.ScannedRobotEvent;
 
 /**
  * @author MyMilkedEek <Michael>
  */
 public class Robocopter extends Robot {
 
+    private boolean enemyScanned = false;
+    private double enemyBearing;
+
     @Override
     public void run() {
         while (true) {
-            ahead(1.0d);
+            if ( !enemyScanned ) {
+                super.turnRadarLeft(10d);
+            }
+
+            if ( this.enemyScanned ) {
+                super.fire(1.0d);
+            }
+        }
+
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+        out.println("enemy scanned");
+        enemyScanned = true;
+
+        double radarHeading = super.getRadarHeading();
+        double gunHeading = super.getGunHeading();
+
+        out.println(radarHeading);
+        out.println(gunHeading);
+        out.println(event.getBearing());
+
+        double b = gunHeading - radarHeading;
+
+        if ( b > 0 ) {
+            turnGunRight(b);
+        } else {
+            turnGunLeft(b);
         }
     }
 }
