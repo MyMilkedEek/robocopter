552b85c4dd2d4ce9798d5293848e9f891cf270a4
Michael Demey <mikkeldemey@gmail.com>
2018-03-01 19:54:12 +0100

    wavesurfing tutorial. Now consistently dodges enemy bullets.

---
+++ src/main/java/net/mymilkedeek/robocopter/EnemyWave.java
@@ -0,0 +1,20 @@
+package net.mymilkedeek.robocopter;
+
+import java.awt.geom.Point2D;
+
+/**
+ * Data class containing information on the enemy and its meta-information
+ *
+ * @author MyMilkedEek <Michael>
+ */
+public class EnemyWave {
+
+    Point2D.Double fireLocation;
+    long fireTime;
+    double bulletVelocity, directAngle, distanceTraveled;
+    int direction;
+
+    public EnemyWave() {
+        // empty body
+    }
+}
